165762df1a86ae3409b9d4d98fa74c35870d04e2
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-03-25 00:28:31 +0100

    Added a commandcontext class and made a constructor for startup

--- Startup.cs
+++ Startup.cs
@@ -5,13 +5,19 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace CmdApi
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public IConfiguration Configuration{get;}
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+
+
+       
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
